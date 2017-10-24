b1ee3623d4172ac415c3f4e4c9f7e34f85876aea
Marco Ruiter <mruiter@tuxweb.nl>
2017-10-24 20:02:32 +0200

    Node info

--- drivers/dresden_rgb/device.js
+++ drivers/dresden_rgb/device.js
@@ -4,6 +4,16 @@ const Homey = require('homey');
 const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
 
 class DresdenRGB extends ZigBeeLightDevice {
+	onMeshInit() {
+
+		// enable debugging
+		this.enableDebug();
+
+		// print the node's info to the console
+		this.printNode();
+
+	}
+
 }
 
 module.exports = DresdenRGB;
