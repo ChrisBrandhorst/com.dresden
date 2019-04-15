4ba48a7ab5dedf74e1c0b984f81cf9f55ca8770f
Chris Brandhorst <chris.brandhorst@topicus.nl>
2019-04-15 08:21:40 +0200

    Added Phoscon FLS-CT

---
+++ drivers/dresden_phoscon_fls-ct/device.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const Homey = require('homey');
+const ZigBeeXYLightDevice = require('homey-meshdriver').ZigBeeXYLightDevice;
+
+class DresdenPhosconFLSCT extends ZigBeeXYLightDevice {
+}
+module.exports = DresdenPhosconFLSCT;
